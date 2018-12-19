4e0a7b757c5ec8d9b0427384e820f8b10485fab7
Shane Ramnode <Shane.h.1@gmail.com>
2018-12-19 15:27:22 +1100

    Stub Pet methods

--- model.go
+++ model.go
@@ -5,6 +5,14 @@ import (
 	"errors"
 )
 
+// Pet -
+type Pet struct {
+	ID      string  `json:"id"`
+	Name    string  `json:"name"`
+	Species float64 `json:"price"`
+	Owner   string  `json:"owner"`
+}
+
 // Owner -
 type Owner struct {
 	ID        string `json:"id"`
@@ -16,18 +24,26 @@ func (p *Owner) getOwner(db *sql.DB) error {
 	return errors.New("Not implemented")
 }
 
-func (p *Owner) updateOwner(db *sql.DB) error {
-	return errors.New("Not implemented")
-}
-
-func (p *Owner) deleteOwner(db *sql.DB) error {
-	return errors.New("Not implemented")
-}
-
 func (p *Owner) createOwner(db *sql.DB) error {
 	return errors.New("Not implemented")
 }
 
 func getOwners(db *sql.DB, start, count int) ([]Owner, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func (p *Pet) getPet(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func (p *Pet) createPet(db *sql.DB) error {
+	return errors.New("Not implemented")
+}
+
+func getPets(db *sql.DB, start, count int) ([]Pet, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func getOwnersPets(db *sql.DB, owner string, start, count int) ([]Pet, error) {
 	return nil, errors.New("Not implemented")
 }
