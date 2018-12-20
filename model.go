d3dd9f8565ed8db66a4ff40a4d4a491d6e082017
Shane Ramnode <Shane.h.1@gmail.com>
2018-12-20 11:47:05 +1100

    Implement model methods

--- model.go
+++ model.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 // Pet -
 type Pet struct {
-	ID      string  `json:"id"`
-	Name    string  `json:"name"`
-	Species float64 `json:"price"`
-	Owner   string  `json:"owner"`
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Species string `json:"species"`
+	Owner   string `json:"owner"`
 }
 
 // Owner -
@@ -20,30 +19,100 @@ type Owner struct {
 	Surname   string `json:"surname"`
 }
 
-func (p *Owner) getOwner(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (o *Owner) getOwner(db *sql.DB) error {
+	return db.QueryRow("SELECT firstname, surname FROM owner WHERE id=$1",
+		o.ID).Scan(&o.Firstname, &o.Surname)
 }
 
-func (p *Owner) createOwner(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (o *Owner) createOwner(db *sql.DB) error {
+	err := db.QueryRow(
+		"INSERT INTO owner(firstname, surname) VALUES($1, $2) RETURNING id",
+		o.Firstname, o.Surname).Scan(&o.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func getOwners(db *sql.DB, start, count int) ([]Owner, error) {
-	return nil, errors.New("Not implemented")
+func getOwners(db *sql.DB) ([]Owner, error) {
+	rows, err := db.Query("SELECT id, firstname, surname FROM owner")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	owners := []Owner{}
+
+	for rows.Next() {
+		var o Owner
+		if err := rows.Scan(&o.ID, &o.Firstname, &o.Surname); err != nil {
+			return nil, err
+		}
+		owners = append(owners, o)
+	}
+
+	return owners, nil
 }
+
+// Pets
 
 func (p *Pet) getPet(db *sql.DB) error {
-	return errors.New("Not implemented")
+	return db.QueryRow("SELECT name, species, owner FROM pet WHERE id=$1", p.ID).Scan(&p.Name, &p.Species, &p.Owner)
 }
 
 func (p *Pet) createPet(db *sql.DB) error {
-	return errors.New("Not implemented")
+	err := db.QueryRow(
+		"INSERT INTO pet(name, species, owner) VALUES($1, $2, $3) RETURNING id",
+		p.Name, p.Species, p.Owner).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func getPets(db *sql.DB, start, count int) ([]Pet, error) {
-	return nil, errors.New("Not implemented")
+func getPets(db *sql.DB) ([]Pet, error) {
+	rows, err := db.Query("SELECT id, name, species, owner FROM pet")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	pets := []Pet{}
+
+	for rows.Next() {
+		var p Pet
+		if err := rows.Scan(&p.ID, &p.Name, &p.Species, &p.Owner); err != nil {
+			return nil, err
+		}
+		pets = append(pets, p)
+	}
+
+	return pets, nil
 }
 
-func getOwnersPets(db *sql.DB, owner string, start, count int) ([]Pet, error) {
-	return nil, errors.New("Not implemented")
+func (o *Owner) getOwnersPets(db *sql.DB) ([]Pet, error) {
+	rows, err := db.Query("SELECT id, name, species, owner FROM pet WHERE owner = $1", o.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	Pets := []Pet{}
+
+	for rows.Next() {
+		var p Pet
+		if err := rows.Scan(&p.ID, &p.Name, &p.Species, &p.Owner); err != nil {
+			return nil, err
+		}
+		Pets = append(Pets, p)
+	}
+
+	return Pets, nil
 }
